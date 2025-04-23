ed24d9449580862cb085870ba4cf6db919e03eac
Michael Berger <m.gonser@icloud.com>
2025-04-23 07:37:47 +0200

    add jasmine plugin

---
+++ tools/eslint-configs/jasmine.js
@@ -0,0 +1,16 @@
+import tseslint from 'typescript-eslint';
+import { TYPESCRIPT_FILE_PATTERNS, TEST_FILE_PATTERNS } from './util/patterns.js';
+import globals from "globals";
+import jasmine from 'eslint-plugin-jasmine';
+
+export default tseslint.config({
+  files: TEST_FILE_PATTERNS,
+  languageOptions: {
+    globals: {
+      ...globals.jasmine
+    }
+  },
+  plugins: {
+    jasmine
+  }
+})
